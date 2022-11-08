a52765c92baa72574049fdc28663a39c049d7756
ANKURO <diwa.saputra@gmail.com>
2022-11-08 18:36:02 +0700

    Changes DB 1

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,15 +13,9 @@ class UsersTableSeeder extends Seeder
     {
         \App\User::create([
             'name'  => 'Administrator',
-<<<<<<< HEAD
             'email' => 'admin@gmail.com',
             'role' => 'admin',
             'password'  => bcrypt('admin')
-=======
-            'email' => 'admin123@gmail.com',
-            'role' => 'admin',
-            'password'  => bcrypt('123456')
->>>>>>> dbe35dec87949b807d530ce49aefc1cae9f39df8
         ]);
     }
 }
